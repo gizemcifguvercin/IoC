0c58a2a1d707ceabc69240f79d1b1bbf1477716f
gizemcifguvercin <gizemcifguvercin@gmail.com>
2020-05-27 20:50:37 +0300

    Add files via upload

---
+++ Principle/DAL/IConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoCOrnek.DAL
+{
+    public interface IConnection
+    {
+        string GetData();
+    }
+}

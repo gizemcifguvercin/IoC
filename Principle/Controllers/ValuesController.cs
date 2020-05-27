daa44912bfdf5420a7925f3b9975441cab1aeb31
gizemcifguvercin <gizemcifguvercin@gmail.com>
2020-05-27 20:57:43 +0300

    Add files via upload

---
+++ Principle/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using IoCOrnek.DAL;
+using IoCOrnek.Helper;
+using IoCOrnek.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IoCOrnek.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private ConnectionHelper _connectionHelper;
+        public ValuesController()
+        {
+            _connectionHelper = new ConnectionHelper(new MsSQLConnection());
+        }
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return _connectionHelper.GetData();
+        }
+
+    }
+}

809aab03847bade5b43946136c1e7ba5e822097a
gizemcifguvercin <gizemcifguvercin@gmail.com>
2020-05-27 20:23:59 +0300

    Add files via upload

---
+++ Principle/Factory/ConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using IoCOrnek.DAL;
+using IoCOrnek.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoCOrnek.Factory
+{
+    public class ConnectionFactory
+    {
+        public static IConnection GetCon()
+        {
+            return new OracleConnection();
+        }
+    }
+}
---
+++ Principle/Helper/ConnectionHelper.cs
@@ -0,0 +1,23 @@
+﻿using IoCOrnek.DAL;
+using IoCOrnek.Factory;
+using IoCOrnek.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoCOrnek.Helper
+{
+    public class ConnectionHelper
+    {
+        private IConnection _connection;
+        public ConnectionHelper(IConnection connection)
+        {
+            _connection = connection;
+        }
+        public string GetData()
+        {
+            return _connection.GetData();
+        }
+    }
+}
